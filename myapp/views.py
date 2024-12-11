5fbcccfb32451b3df594d200d22aa72413fb3148
abvan <abschavan99@gmail.com>
2024-12-11 17:05:23 +0530

    User Login and dynamic HTML

--- myapp/views.py
+++ myapp/views.py
@@ -14,6 +14,7 @@ def counter(request):
     amt_of_words = len(text.split())
     return render(request,'counter.html',{'amount':amt_of_words})
 
+
 def register(request):
     
     if request.method == 'POST' :
@@ -38,3 +39,21 @@ def register(request):
             return redirect('register')
     else :
         return render(request,'register.html')
+
+def login(request):
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username , password = password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else :
+            messages.info(request,'Credentials Invalid')
+            return redirect('login')
+    else :
+        return render(request,'login.html')
+
+    return render(request,'login.html')
