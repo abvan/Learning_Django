5c9512e10fe661c04e114ce085ef32f9e4e1e373
abvan <abschavan99@gmail.com>
2024-12-11 14:00:59 +0530

    User Regisration in Django

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -11,3 +13,28 @@ def counter(request):
     text = request.POST['text']
     amt_of_words = len(text.split())
     return render(request,'counter.html',{'amount':amt_of_words})
+
+def register(request):
+    
+    if request.method == 'POST' :
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2 :
+            if User.objects.filter(email = email).exists() :
+                messages.info(request,'Email Already in use')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username already exists')
+                return redirect('register')
+            else :
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+        else :
+            messages.info(request,'Password Not The Same')
+            return redirect('register')
+    else :
+        return render(request,'register.html')
