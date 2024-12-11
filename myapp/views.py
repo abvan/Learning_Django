aa0e2ae0096b6359fd262123a7fb830e62df4d89
abvan <abschavan99@gmail.com>
2024-12-11 11:03:18 +0530

    Dynamic HTML with Models and For-Loop

--- myapp/views.py
+++ myapp/views.py
@@ -8,7 +8,24 @@ def index(request):
     feature1.id = 0
     feature1.name = 'fast'
     feature1.details = 'Our service is very quick'
-    return render(request,'index.html',{'feature':feature1})
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service is Reliable'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Accurate'
+    feature3.details = 'Our service is Accurate'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Modern'
+    feature4.details = 'Our service is Modern'
+
+    features = [feature1,feature2,feature3,feature4]
+    return render(request,'index.html',{'features':features})
 
 def counter(request):
     text = request.POST['text']
