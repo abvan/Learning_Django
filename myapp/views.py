2bd8d4047f97362e37fb1d27017e16acbf06fd9b
abvan <abschavan99@gmail.com>
2024-12-11 13:13:47 +0530

    registered Models Created Admin User ,Used Database to retrive data in Dynamic HTML

--- myapp/views.py
+++ myapp/views.py
@@ -4,27 +4,7 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'fast'
-    feature1.details = 'Our service is very quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.details = 'Our service is Reliable'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Accurate'
-    feature3.details = 'Our service is Accurate'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Modern'
-    feature4.details = 'Our service is Modern'
-
-    features = [feature1,feature2,feature3,feature4]
+    features = Feature.objects.all
     return render(request,'index.html',{'features':features})
 
 def counter(request):
